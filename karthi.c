d79dea47c93536dc5d31e793693a2e26e7591b2b
sakthigowri01 <35915299+sakthigowri01@users.noreply.github.com>
2018-02-28 14:19:59 +0530

    karthi.c

---
+++ karthi.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void) {
+	int n;
+	scanf("%d",&n);
+	while(n!=0)
+	{
+		if(n%10==1)
+		{
+			printf("%d",n);
+			break;
+		}
+		n++;
+	}
+	return 0;
+}
