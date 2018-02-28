bbd9a2143ab0a303450c1194e397af3ff52263da
sakthigowri01 <35915299+sakthigowri01@users.noreply.github.com>
2018-02-28 14:50:53 +0530

    jeevi.c

---
+++ jeevi.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+  int n,l,r;
+  printf("Enter the number");
+  scanf("%d",&n);
+  printf("Enter he value for L and R");
+  scanf("%d\n%d",&l,&2,vi.c&r);
+  if(n>=l&&n<=r)
+  {
+    printf("YES");
+  }
+  else 
+  {
+    printf("NO");
+  }
+  return 0;
+}
